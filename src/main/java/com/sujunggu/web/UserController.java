b2bb6a7285b9a893c71d85a10ebdba07474e43bc
congcoding <congcoding@gmail.com>
2020-07-20 18:12:13 +0900

    AWS에서 배포했을 때 경로 못찾아서 / 삭제 (/index -> index)

--- src/main/java/com/sujunggu/web/UserController.java
+++ src/main/java/com/sujunggu/web/UserController.java
@@ -16,45 +16,45 @@ public class UserController {
     // 메인 페이지
     @GetMapping("/")
     public String index() {
-        return "/index";
+        return "index";
     }
 
     // 회원가입 페이지
     @GetMapping("/user/signup")
     public String dispSignup() {
-        return "/signup";
+        return "signup";
     }
 
     // 회원가입 처리
     @PostMapping("/user/signup")
     public String execSignup(UserDto userDto) {
         userService.joinUser(userDto);
-        return "/signup";
+        return "signup";
     }
 
     // 이메일 인증 처리
     @GetMapping("/user/active")
     public String execActive(UserDto userDto) {
         userService.updateActive(userDto);
-        return "/active";
+        return "active";
     }
 
     // 비밀번호 찾기 페이지
     @GetMapping("/user/find-password")
-    public String dispFindPassword() { return "/findpassword"; }
+    public String dispFindPassword() { return "findpassword"; }
 
     // 비밀번호 찾기
     @PostMapping("/user/send-temp-password")
     public String execSendTempPassword(String email) {
         String tempPassword = userService.updateTempPassword(email);
         userService.sendTempPassword(email, tempPassword);
-        return "/findpassword";
+        return "findpassword";
     }
 
     // 접근 거부 페이지
     @GetMapping("/user/denied")
     public String dispDenied() {
-        return "/denied";
+        return "denied";
     }
 
 }
