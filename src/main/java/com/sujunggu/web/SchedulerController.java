5fa7469bbdc6e6922bb5cc5698545c7c16b26dc9
congcoding <congcoding@gmail.com>
2020-07-20 17:16:48 +0900

    cron 정각으로 수정

--- src/main/java/com/sujunggu/web/SchedulerController.java
+++ src/main/java/com/sujunggu/web/SchedulerController.java
@@ -18,7 +18,7 @@ public class SchedulerController {
     // cron : 초(0-59) 분(0-59) 시간(0-23) 일(1-31) 월(1-12) 요일(0-7 일월화수목금토)
     // 0 * * * * * : 매 0초에 실행 (1분 주기)
     // 0 0 * * * * : 매 0분에 실행 (1시간 주기)
-    @Scheduled(cron = "0 11 * * * *")
+    @Scheduled(cron = "0 0 * * * *")
     public void scheduling() throws IOException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date now = new Date();
